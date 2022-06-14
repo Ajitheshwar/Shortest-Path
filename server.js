7d20051eb204701670c2bd23fde9b77daaa4e4a4
Ajitheshwar <ajitheshwar1923@gmail.com>
2022-06-15 02:15:04 +0530

    final

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const exp = require("express")
 const app = exp()
 const path = require("path")
+app.use(exp.static(path.join(__dirname,"game")))
+const port = 4000
 app.listen(port, ()=>{console.log(`App listening on port ${port}`)})
 app.use(exp.json())
 
 app.get("",async(req,res,next)=>{
-    res.render(path.join(__dirname+'/game.html'))
+    res.sendFile(path.join(__dirname+'/game/game.html'))
 })
